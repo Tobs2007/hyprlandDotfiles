05ae0bc751aaac34b2f3f2d7561f044ff20915b1
Tobs2007 <tobias.rommert@gmx.de>
2025-06-16 18:55:48 +0000

    a

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-cd ~/dotfiles
-export XDG_CONFIG_HOME=~/dotfiles/
-pacman -S python3 wofi waybar
+echo installing hyprland
+cd ~
+wget -r https://tobsstuff.com/data/dotfiles
+mkdir ~/.config/hypr
+cd ~/.config/hypr
+wget https://tobsstuff.com/data/dotfiles/hypr/redirect/hyprland.config
+
+sudo pacman -S python3 wofi waybar hyprland kitty code
+echo export XDG_CONFIG_HOME=~/dotfiles/ > ~/.bashrc
 python -m venv ~/dotfiles/.venv
 source ~/dotfiles/.venv/bin/activate
 pip install psutil
